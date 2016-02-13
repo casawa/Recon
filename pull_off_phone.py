b804e7b55736ba6e93e7648723cb74ae22868343
Andrew Chen <awc8888@gmail.com>
2016-02-13 00:35:50 -0800

    added

--- pull_off_phone.py
+++ pull_off_phone.py
@@ -1,9 +1,9 @@
 import os
 import time
 
-# i = 0
+i = 0
 while True:
 	os.system("adb shell screencap /sdcard/screen.png & adb pull /sdcard/screen.png")
-	# os.system("mv screen.png pics/" + str(i))
-	# i+=1
+	os.system("mv screen.png pics/" + str(i) + ".png") 
+	i+=1
 	time.sleep(6)	# Delays for 6 seconds
