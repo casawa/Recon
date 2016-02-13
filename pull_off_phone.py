27c1993f22fb3f7316ce0a40742acbac26966729
Andrew Chen <awc8888@gmail.com>
2016-02-12 23:25:09 -0800

    image storage

--- pull_off_phone.py
+++ pull_off_phone.py
@@ -1,5 +1,8 @@
 import os
 
+i = 0
 while True:
 	os.system("adb shell screencap /sdcard/screen.png & adb pull /sdcard/screen.png")
+	os.system("mv screen.png pics/" + str(i))
+	i+=1
 	os.sleep(600)
