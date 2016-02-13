3f2f271ed101e77349a408f6794f9c22f1f42441
Chaitanya Asawa <casawa@stanford.edu>
2016-02-13 00:52:00 -0800

    Pulling off phone works with classifying images

--- pull_off_phone.py
+++ pull_off_phone.py
@@ -5,5 +5,6 @@ i = 0
 while True:
 	os.system("adb shell screencap /sdcard/screen.png & adb pull /sdcard/screen.png")
 	os.system("mv screen.png pics/" + str(i) + ".png") 
-	i+=1
+	i += 1
+	os.system("sh WatsonAPI/classify.sh pics/" + str(i) + ".png")
 	time.sleep(6)	# Delays for 6 seconds
