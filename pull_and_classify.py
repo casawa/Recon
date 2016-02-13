123fc4301568874543914f8dc1c7979e1db82bc5
Chaitanya Asawa <casawa@stanford.edu>
2016-02-13 01:07:10 -0800

    New script for just pulling

--- pull_and_classify.py
+++ pull_and_classify.py
@@ -8,4 +8,5 @@ while True:
 	os.system("adb pull /sdcard/screen.png")
 	os.system("mv screen.png pics/" + str(i) + ".png") 
 	i += 1
+	os.system("sh WatsonAPI/classify.sh pics/" + str(i) + ".png")
 	time.sleep(6)	# Delays for 6 seconds
