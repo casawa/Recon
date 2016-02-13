baaea3c236612061ee3895fcc6efb853b1e69026
Marcus Vincent Gomez <mvgomez@stanford.edu>
2016-02-13 00:30:27 -0800

    fixed watson_init.py

--- WatsonAPI/watson_init.py
+++ WatsonAPI/watson_init.py
@@ -1,11 +1,15 @@
 import os, subprocess
 import sys
 
-os.system("sh get_token.sh")
-output = subprocess.Popen("sh upload.sh", stdout=subprocess.PIPE).communicate()[0]
-classifier_info = eval(output)
-class_id = classifier_info["classifier_id"]
+if len(sys.argv) > 3:
+	os.system("sh get_token.sh")
+	output = subprocess.Popen("sh upload_images.sh " + sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3], \
+													stdout=subprocess.PIPE, shell=True).communicate()[0]
+	classifier_info = eval(output)
+	class_id = classifier_info["classifier_id"]
 
-with open('classifierlist.json', 'w') as f:	
-	file_str = '{"classifier_ids": ["' + class_id + '"]}'
-	f.write(file_str)
+	with open('classifierlist.json', 'w') as f:
+		file_str = '{"classifier_ids": ["' + class_id + '"]}\n'
+		f.write(file_str)
+else:
+	print "args yo"
