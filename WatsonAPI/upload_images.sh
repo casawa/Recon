c02daf86aefb31c5d678abfe1441fd04a33ad8de
Marcus Vincent Gomez <mvgomez@stanford.edu>
2016-02-12 23:59:37 -0800

    updated watson code

--- WatsonAPI/upload_images.sh
+++ WatsonAPI/upload_images.sh
@@ -1,8 +1,6 @@
-
-
 curl -u "1c0927e5-5d95-458d-a0a0-5b0f06d23ae5":"efl5ZftB7WOI" \
 	     -X POST \
 	          -F "positive_examples=@$1" \
 		       -F "negative_examples=@$2" \
-		            -F "name=tiger" \
+		            -F "name=$3" \
 			         "https://gateway.watsonplatform.net/visual-recognition-beta/api/v2/classifiers?version=2015-12-02"
