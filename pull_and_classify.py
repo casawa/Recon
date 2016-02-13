176f1b322f90985dbc40413378c57c0e1655df12
Marcus Vincent Gomez <mvgomez@stanford.edu>
2016-02-13 13:59:28 -0800

    updated scripts for pull

--- pull_and_classify.py
+++ pull_and_classify.py
@@ -4,9 +4,7 @@ import time
 i = 0
 while True:
 	os.system("adb shell screencap /sdcard/screen.png")
-	time.sleep(0.5)
 	os.system("adb pull /sdcard/screen.png")
 	os.system("mv screen.png pics/" + str(i) + ".png") 
 	i += 1
 	os.system("sh WatsonAPI/classify.sh pics/" + str(i) + ".png")
-	time.sleep(6)	# Delays for 6 seconds
