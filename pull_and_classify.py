da4e1d9a8ee8b528cf5d2f931765b274107d7e1e
Marcus Vincent Gomez <mvgomez@stanford.edu>
2016-02-13 19:16:55 -0800

    updated pull/classify

--- pull_and_classify.py
+++ pull_and_classify.py
@@ -1,6 +1,7 @@
 import os
 import time
-from 
+import subprocess
+import json
 
 # vals post crop
 LEFT = 0
@@ -15,7 +16,7 @@ i = 0
 while True:
 	os.system("adb shell screencap /sdcard/screen.png")
 	os.system("adb pull /sdcard/screen.png")
-	os.system("mv screen.png pics/" + str(i) + ".png") 
+	os.system("mv screen.png pics/" + str(i) + ".png")
 	i += 1
 	im = im.crop((140, 250, 940, 1640))
 
@@ -25,11 +26,22 @@ while True:
 	newHeight = height / num
 
 	positive = False
-    for x in range(num):
-    	for y in range(num):
-    		print ("%d, %d, %d, %d saved as %s" % (LEFT + x * newWidth, TOP + y * newHeight, \
-    				 RIGHT - (num-x - 1) * newWidth, BOTTOM - (num-y - 1) * newHeight, str(x) + str(y) + i))
-    		new = im.crop((LEFT + x * newWidth, TOP + y * newHeight, \
-    				RIGHT - (num-x-1) * newWidth, BOTTOM - (num-y-1) * newHeight))
-    		new.save("pics/" + str(i) + "_" + str(x) + str(y) + ".png")
-			os.system("sh WatsonAPI/classify.sh pics/" + str(i) + "_" + str(x) + str(y) + ".png")
+	for x in range(num):
+		for y in range(num):
+			print ("%d, %d, %d, %d saved as %s" % (LEFT + x * newWidth, TOP + y * newHeight, \
+ 				RIGHT - (num-x - 1) * newWidth, BOTTOM - (num-y - 1) * newHeight, str(x) + str(y) + i))
+			new = im.crop((LEFT + x * newWidth, TOP + y * newHeight, \
+				RIGHT - (num-x-1) * newWidth, BOTTOM - (num-y-1) * newHeight))
+			new.save("pics/" + str(i) + "_" + str(x) + str(y) + ".png")
+			v = subprocess.check_output("sh WatsonAPI/classify.sh pics/" + str(i) + "_" + str(x) + str(y) + ".png", shell=True)
+			j = json.loads(v)
+			if "images" in j:
+				arr = j["images"]
+				cur = arr[0]
+				if "scores" in cur:
+					positive = True
+					break
+	if positive:
+		print "EYYY LMAO FUCK YOUR MOTHER ~ANDREW CHEN 2016"
+	else:
+		print "EYYY LMAO FUCK YOUR MOTHER (IN THE BAD WAY)"
